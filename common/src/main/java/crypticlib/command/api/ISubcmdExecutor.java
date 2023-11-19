e059302d41619e4098581dfde710b278deb07bf6
Yufiria <xiaoyi357159@gmail.com>
2023-11-19 09:47:24 +0800

    [0.2.0]重写命令模块,修复bug

---
+++ common/src/main/java/crypticlib/command/api/ISubcmdExecutor.java
@@ -0,0 +1,29 @@
+package crypticlib.command.api;
+
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+import java.util.function.BiFunction;
+
+public interface ISubcmdExecutor extends ICmdExecutor {
+
+    String name();
+
+    List<String> aliases();
+
+    String permission();
+
+    @Override
+    default ISubcmdExecutor regSub(ISubcmdExecutor subcmdExecutor) {
+        return (ISubcmdExecutor) ICmdExecutor.super.regSub(subcmdExecutor);
+    }
+
+    @Override
+    default ISubcmdExecutor regSub(String name, BiFunction<CommandSender, List<String>, Boolean> executor) {
+        return (ISubcmdExecutor) ICmdExecutor.super.regSub(name, executor);
+    }
+
+    @Override
+    ISubcmdExecutor setExecutor(BiFunction<CommandSender, List<String>, Boolean> executor);
+
+}
