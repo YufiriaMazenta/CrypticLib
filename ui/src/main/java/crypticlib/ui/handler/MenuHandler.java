b480683b75e7a2f7cf1ac14d089a6199c3f2c130
ChiyodaXiaoYi <linjiayi357159@gmail.com>
2023-11-23 11:29:16 +0800

    [0.2.5]拆分模块,开始编写箱子UI相关功能

---
+++ ui/src/main/java/crypticlib/ui/handler/MenuHandler.java
@@ -0,0 +1,38 @@
+package crypticlib.ui.handler;
+
+import crypticlib.listener.BukkitListener;
+import crypticlib.ui.Menu;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.inventory.InventoryHolder;
+
+@BukkitListener
+public class MenuHandler implements Listener {
+
+    @EventHandler
+    public void onClickMenu(InventoryClickEvent event) {
+        if (event.getClickedInventory() == null)
+            return;
+        InventoryHolder holder = event.getView().getTopInventory().getHolder();
+        if (!(holder instanceof Menu))
+            return;
+        ((Menu) holder).click(event.getSlot(), event);
+    }
+
+    @EventHandler
+    public void onDragMenu(InventoryDragEvent event) {
+        if (event.getView().getTopInventory().getHolder() instanceof Menu)
+            event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onCloseMenu(InventoryCloseEvent event) {
+        if (!(event.getView().getTopInventory().getHolder() instanceof Menu))
+            return;
+        ((Menu) event.getView().getTopInventory().getHolder()).onClose(event);
+    }
+
+}
