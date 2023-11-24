67475d7d874748b7497e3d802d26bdfb5e0bb7a7
Yufiria <xiaoyi357159@gmail.com>
2023-11-24 20:21:44 +0800

    [0.3.0-dev3]修复ui模块几个刷物品或者导致物品丢失的bug

--- ui/src/main/java/crypticlib/ui/handler/MenuHandler.java
+++ ui/src/main/java/crypticlib/ui/handler/MenuHandler.java
@@ -2,13 +2,16 @@ package crypticlib.ui.handler;
 
 import crypticlib.listener.BukkitListener;
 import crypticlib.ui.Menu;
+import crypticlib.ui.StoredMenu;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
 
 @BukkitListener
 public class MenuHandler implements Listener {
@@ -25,8 +28,16 @@ public class MenuHandler implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onDragMenu(InventoryDragEvent event) {
-        if (event.getView().getTopInventory().getHolder() instanceof Menu)
-            event.setCancelled(true);
+        if (!(event.getView().getTopInventory().getHolder() instanceof Menu)) {
+            return;
+        }
+        for (Integer slot : event.getInventorySlots()) {
+            ItemStack current = event.getInventory().getItem(slot);
+            if (event.getOldCursor().isSimilar(current)) {
+                event.setCancelled(true);
+                return;
+            }
+        }
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
@@ -34,6 +45,14 @@ public class MenuHandler implements Listener {
         if (!(event.getView().getTopInventory().getHolder() instanceof Menu))
             return;
         ((Menu) event.getView().getTopInventory().getHolder()).onClose(event);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        InventoryHolder inventoryHolder = event.getPlayer().getInventory().getHolder();
+        if (inventoryHolder instanceof StoredMenu) {
+            ((StoredMenu) inventoryHolder).refreshStoredItems(event.getPlayer().getInventory()).returnStoredItems();
+        }
     }
 
 }
