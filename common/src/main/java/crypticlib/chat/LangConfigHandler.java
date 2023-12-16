e47b207a8a9c1effeec516e1d5079a41198cc3cb
YufiriaMazenta <2199098065@qq.com>
2023-12-16 11:21:09 +0800

    [0.8.1]多语言模块添加Locale相关方法

--- common/src/main/java/crypticlib/chat/LangConfigHandler.java
+++ common/src/main/java/crypticlib/chat/LangConfigHandler.java
@@ -1,5 +1,10 @@
 package crypticlib.chat;
 
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface LangConfigHandler {
 
     String langFileFolder();
