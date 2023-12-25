e2905c73cf55cbf6642f2ddf667a7e418754706b
YufiriaMazenta <2199098065@qq.com>
2023-12-25 16:34:05 +0800

    [0.10.8]ItemUtil添加一些工具方法

--- common/src/main/java/crypticlib/util/ItemUtil.java
+++ common/src/main/java/crypticlib/util/ItemUtil.java
@@ -1,7 +1,12 @@
 package crypticlib.util;
 
+import com.google.common.base.Preconditions;
+import crypticlib.chat.TextProcessor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
 
 /**
  * 物品相关工具类
@@ -16,6 +21,35 @@ public class ItemUtil {
      */
     public static boolean isAir(ItemStack item) {
         return item == null || item.getType().equals(Material.AIR);
+    }
+
+    public static void setDisplayName(ItemStack item, String displayName) {
+        setDisplayName(item, displayName, true);
+    }
+
+    public static void setDisplayName(ItemStack item, String displayName, boolean format) {
+        Preconditions.checkArgument(!ItemUtil.isAir(item), "Item can not be null");
+        ItemMeta meta = item.getItemMeta();
+        if (format) {
+            displayName = TextProcessor.color(displayName);
+        }
+        meta.setDisplayName(displayName);
+        item.setItemMeta(meta);
+    }
+
+    public static void setLore(ItemStack item, List<String> lore) {
+        setLore(item, lore, true);
+    }
+
+    public static void setLore(ItemStack item, List<String> lore, boolean format) {
+        Preconditions.checkArgument(!ItemUtil.isAir(item), "Item can not be air");
+        ItemMeta itemMeta = item.getItemMeta();
+        if (format) {
+            lore.replaceAll(TextProcessor::color);
+        }
+        itemMeta.setLore(lore);
+
+        item.setItemMeta(itemMeta);
     }
 
 }
