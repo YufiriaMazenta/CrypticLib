2933ea165b09294e3dc602a43a42804ea4efb623
YufiriaMazenta <2199098065@qq.com>
2024-01-17 17:45:11 +0800

    [0.10.33]CommandManager更改存储方式，添加unregisterCommand方法

--- common/src/main/java/crypticlib/command/CommandManager.java
+++ common/src/main/java/crypticlib/command/CommandManager.java
@@ -1,7 +1,6 @@
 package crypticlib.command;
 
 import crypticlib.CrypticLib;
-import crypticlib.perm.PermissionManager;
 import crypticlib.util.ReflectUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -10,12 +9,13 @@ import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public enum CommandManager {
 
@@ -23,20 +23,15 @@ public enum CommandManager {
 
     private final CommandMap serverCommandMap;
     private final Constructor<?> pluginCommandConstructor;
-    private final List<Command> registeredCommands;
+    private final Map<String, Map<String, Command>> registeredCommands = new ConcurrentHashMap<>();
 
     CommandManager() {
         Method getCommandMapMethod = ReflectUtil.getMethod(Bukkit.getServer().getClass(), "getCommandMap");
         serverCommandMap = (CommandMap) ReflectUtil.invokeMethod(getCommandMapMethod, Bukkit.getServer());
         pluginCommandConstructor = ReflectUtil.getDeclaredConstructor(PluginCommand.class, String.class, Plugin.class);
-        registeredCommands = new CopyOnWriteArrayList<>();
     }
 
-    public static SubcmdExecutor subcommand(@NotNull String name) {
-        return new SubcmdExecutor(name);
-    }
-
-    public CommandManager register(@NotNull Plugin plugin, @NotNull CommandInfo commandInfo, @NotNull TabExecutor commandExecutor) {
+    public Command register(@NotNull Plugin plugin, @NotNull CommandInfo commandInfo, @NotNull TabExecutor commandExecutor) {
         PluginCommand pluginCommand = (PluginCommand) ReflectUtil.invokeDeclaredConstructor(pluginCommandConstructor, commandInfo.name(), plugin);
         pluginCommand.setAliases(Arrays.asList(commandInfo.aliases()));
         pluginCommand.setDescription(commandInfo.description());
@@ -44,18 +39,63 @@ public enum CommandManager {
         pluginCommand.setUsage(commandInfo.usage());
         pluginCommand.setExecutor(commandExecutor);
         pluginCommand.setTabCompleter(commandExecutor);
-        serverCommandMap.register(plugin.getName(), pluginCommand);
-        registeredCommands.add(pluginCommand);
+        String pluginName = plugin.getName();
+        serverCommandMap.register(pluginName, pluginCommand);
+        if (registeredCommands.containsKey(pluginName)) {
+            Map<String, Command> commandMap = registeredCommands.get(pluginName);
+            commandMap.put(commandInfo.name(), pluginCommand);
+        } else {
+            Map<String, Command> commandMap = new ConcurrentHashMap<>();
+            commandMap.put(commandInfo.name(), pluginCommand);
+            registeredCommands.put(pluginName, commandMap);
+        }
         CrypticLib.permissionManager().regPerm(commandInfo.permission(), commandInfo.permDef());
-        return this;
+        return pluginCommand;
     }
 
-    public CommandManager unregisterAll() {
-        for (Command registeredCommand : registeredCommands) {
-            registeredCommand.unregister(serverCommandMap);
+    /**
+     * 注销一个命令
+     * @param plugin 命令所属的插件
+     * @param commandName 命令的名字
+     * @return 被注销的命令，若为null即不存在此命令
+     */
+    @Nullable
+    public Command unregister(Plugin plugin, String commandName) {
+        String pluginName = plugin.getName();
+        if (!registeredCommands.containsKey(pluginName))
+            return null;
+        Map<String, Command> commandMap = registeredCommands.get(commandName);
+        Command command = commandMap.get(pluginName);
+        if (command == null)
+            return null;
+        command.unregister(serverCommandMap);
+        commandMap.remove(commandName);
+        if (commandMap.isEmpty()) {
+            registeredCommands.remove(pluginName);
         }
+        return command;
+    }
+
+    /**
+     * 注销所有通过CommandManager的命令
+     */
+    public void unregisterAll() {
+        registeredCommands.forEach(
+            (plugin, commandMap) -> {
+                for (Command command : commandMap.values()) {
+                    command.unregister(serverCommandMap);
+                }
+            }
+        );
         registeredCommands.clear();
-        return this;
+    }
+
+    public Map<String, Map<String, Command>> registeredCommands() {
+        return registeredCommands;
+    }
+
+    public static SubcmdExecutor subcommand(@NotNull String name) {
+        return new SubcmdExecutor(name);
     }
 
 }
