49c8a4a618a6889f0edd4b7f893d09f8493c5298
YufiriaMazenta <2199098065@qq.com>
2024-02-01 18:27:13 +0800

    [0.12.1]更改CrypticLibRunnable类实现Runnable接口

--- common/src/main/java/crypticlib/scheduler/CrypticLibRunnable.java
+++ common/src/main/java/crypticlib/scheduler/CrypticLibRunnable.java
@@ -5,94 +5,82 @@ import crypticlib.scheduler.task.ITaskWrapper;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.plugin.Plugin;
-import org.jetbrains.annotations.NotNull;
 
-public class CrypticLibRunnable {
+public abstract class CrypticLibRunnable implements Runnable {
 
     protected ITaskWrapper taskWrapper;
-    protected Runnable runnable;
 
-    public CrypticLibRunnable(@NotNull Runnable runnable) {
-        this.runnable = runnable;
-    }
+    @Override
+    public abstract void run();
 
     public ITaskWrapper runTask(Plugin plugin) {
-        checkRunnable();
-        return setTaskWrapper(CrypticLib.platform().scheduler().runTask(plugin, runnable));
+        checkTaskNotNull();
+        return setTaskWrapper(CrypticLib.platform().scheduler().runTask(plugin, this));
     }
 
     public ITaskWrapper runTaskLater(Plugin plugin, long delayTicks) {
-        checkRunnable();
-        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskLater(plugin, runnable, delayTicks));
+        checkTaskNotNull();
+        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskLater(plugin, this, delayTicks));
     }
 
     public ITaskWrapper runTaskTimer(Plugin plugin, long delayTicks, long periodTicks) {
-        checkRunnable();
-        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskTimer(plugin, runnable, delayTicks, periodTicks));
+        checkTaskNotNull();
+        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskTimer(plugin, this, delayTicks, periodTicks));
     }
 
     public ITaskWrapper runTaskAsync(Plugin plugin) {
-        checkRunnable();
-        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskAsync(plugin, runnable));
+        checkTaskNotNull();
+        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskAsync(plugin, this));
     }
 
     public ITaskWrapper runTaskLaterAsync(Plugin plugin, long delayTicks) {
-        checkRunnable();
-        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskLaterAsync(plugin, runnable, delayTicks));
+        checkTaskNotNull();
+        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskLaterAsync(plugin, this, delayTicks));
     }
 
     public ITaskWrapper runTaskTimerAsync(Plugin plugin, long delayTicks, long periodTicks) {
-        checkRunnable();
-        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskTimerAsync(plugin, runnable, delayTicks, periodTicks));
+        checkTaskNotNull();
+        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskTimerAsync(plugin, this, delayTicks, periodTicks));
     }
 
     public ITaskWrapper runTaskOnLocation(Plugin plugin, Location location) {
-        checkRunnable();
-        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskOnLocation(plugin, location, runnable));
+        checkTaskNotNull();
+        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskOnLocation(plugin, location, this));
     }
 
     public ITaskWrapper runTaskOnLocationLater(Plugin plugin, Location location, long delayTicks) {
-        checkRunnable();
-        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskOnLocationLater(plugin, location, runnable, delayTicks));
+        checkTaskNotNull();
+        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskOnLocationLater(plugin, location, this, delayTicks));
     }
 
     public ITaskWrapper runTaskOnLocationTimer(Plugin plugin, Location location, long delayTicks, long periodTicks) {
-        checkRunnable();
-        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskOnLocationTimer(plugin, location, runnable, delayTicks, periodTicks));
+        checkTaskNotNull();
+        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskOnLocationTimer(plugin, location, this, delayTicks, periodTicks));
     }
 
     public ITaskWrapper runTaskOnEntity(Plugin plugin, Entity entity) {
-        checkRunnable();
-        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskOnEntity(plugin, entity, runnable, runnable));
+        checkTaskNotNull();
+        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskOnEntity(plugin, entity, this, this));
     }
 
     public ITaskWrapper runTaskOnEntityLater(Plugin plugin, Entity entity, long delayTicks) {
-        checkRunnable();
-        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskOnEntityLater(plugin, entity, runnable, runnable, delayTicks));
+        checkTaskNotNull();
+        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskOnEntityLater(plugin, entity, this, this, delayTicks));
     }
 
     public ITaskWrapper runTaskOnEntityTimer(Plugin plugin, Entity entity, long delayTicks, long periodTicks) {
-        checkRunnable();
-        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskOnEntityTimer(plugin, entity, runnable, runnable, delayTicks, periodTicks));
+        checkTaskNotNull();
+        return setTaskWrapper(CrypticLib.platform().scheduler().runTaskOnEntityTimer(plugin, entity, this, this, delayTicks, periodTicks));
     }
 
     public void cancel() {
-        checkTask();
+        checkTaskNull();
         this.taskWrapper.cancel();
     }
 
     public boolean isCancelled() {
-        checkTask();
+        checkTaskNull();
         return this.taskWrapper.isCancelled();
-    }
-
-    public Runnable runnable() {
-        return runnable;
-    }
-
-    public CrypticLibRunnable setRunnable(Runnable runnable) {
-        this.runnable = runnable;
-        return this;
     }
 
     protected ITaskWrapper setTaskWrapper(ITaskWrapper taskWrapper) {
@@ -100,13 +88,13 @@ public class CrypticLibRunnable {
         return this.taskWrapper;
     }
 
-    protected void checkRunnable() {
-        if (this.runnable == null) {
+    protected void checkTaskNotNull() {
+        if (this.taskWrapper != null) {
             throw new IllegalArgumentException("Runnable is null");
         }
     }
 
-    protected void checkTask() {
+    protected void checkTaskNull() {
         if (this.taskWrapper == null) {
             throw new IllegalArgumentException("Task is null");
         }
