2d628dc808c1523d4abf6ceaea8acf84fdf08370
YufiriaMazenta <2199098065@qq.com>
2024-02-03 22:06:50 +0800

    [0.14.1]修改一些错误的变量名和注释

--- common/src/main/java/crypticlib/command/annotation/CommandTree.java
+++ common/src/main/java/crypticlib/command/annotation/CommandTree.java
@@ -3,8 +3,8 @@ package crypticlib.command.annotation;
 import java.lang.annotation.*;
 
 /**
- * 用于插件加载时自动注册命令的注解，拥有此注解的命令类将会被自动注册，无需在plugin.yml中声明
- * 此注解只能用在RootCommand类上
+ * 用于标识命令树的注解，拥有此注解的命令树将会被自动注册为插件命令，无需在plugin.yml中声明
+ * 此注解只能用在CommandTreeRoot类上
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
