6833ab9443d862c97f388ee9efa21ba1127d91ff
YufiriaMazenta <linjiayi357159@gmail.com>
2024-07-22 09:31:01 +0800

    [1.0.7]新增类IconDisplay

--- module/bukkit/ui/src/main/java/crypticlib/ui/display/Icon.java
+++ module/bukkit/ui/src/main/java/crypticlib/ui/display/Icon.java
@@ -16,82 +16,23 @@ public class Icon {
     private ItemStack display;
     private Consumer<InventoryClickEvent> clickAction;
 
-    public Icon(@NotNull Material material) {
-        this(new ItemStack(material), null, null, null);
-    }
-
-    public Icon(@NotNull Material material, @Nullable Integer customModelData) {
-        this(new ItemStack(material), null, null, customModelData);
-    }
-
-    public Icon(@NotNull Material material, @Nullable String name) {
-        this(new ItemStack(material), name, null, null);
-    }
-
-    public Icon(@NotNull Material material, @Nullable List<String> lore) {
-        this(new ItemStack(material), null, lore, null);
-    }
-
-    public Icon(@NotNull Material material, @Nullable List<String> lore, @Nullable Integer customModelData) {
-        this(new ItemStack(material), null, lore, customModelData);
-    }
-
-    public Icon(@NotNull Material material, @Nullable String name, @Nullable Integer customModelData) {
-        this(new ItemStack(material), name, null, customModelData);
-    }
-
-    public Icon(@NotNull Material material, @Nullable String name, @Nullable List<String> lore) {
-        this(new ItemStack(material), name, lore, null);
-    }
-
-    public Icon(@NotNull Material material, @Nullable String name, @Nullable List<String> lore, @Nullable Integer customModelData) {
-        this(new ItemStack(material), name, lore, customModelData);
+    public Icon(@NotNull IconDisplay iconDisplay) {
+        this.display = iconDisplay.display();
     }
 
     public Icon(@NotNull ItemStack display) {
-        this(display, null, null, null);
+        this(display, null);
     }
 
-    public Icon(@NotNull ItemStack display, @Nullable String name) {
-        this(display, name, null, null);
-    }
-
-    public Icon(@NotNull ItemStack display, @Nullable List<String> lore) {
-        this(display, null, lore, null);
-    }
-
-    public Icon(@NotNull ItemStack display, @Nullable Integer customModelData) {
-        this(display, null, null, customModelData);
-    }
-
-    public Icon(@NotNull ItemStack display, @Nullable List<String> lore, @Nullable Integer customModelData) {
-        this(display, null, lore, customModelData);
-    }
-
-    public Icon(@NotNull ItemStack display, @Nullable String name, @Nullable Integer customModelData) {
-        this(display, name, null, customModelData);
-    }
-
-    public Icon(@NotNull ItemStack display, @Nullable String name, @Nullable List<String> lore) {
-        this(display, name, lore, null);
-    }
-
-    public Icon(@NotNull ItemStack display, @Nullable String name, @Nullable List<String> lore, @Nullable Integer customModelData) {
-        if (!ItemHelper.isAir(display)) {
-            ItemStack displayClone = display.clone();
-            ItemMeta itemMeta = displayClone.getItemMeta();
-            if (itemMeta == null)
-                throw new IllegalArgumentException("Item meta can not be null");
-            if (name != null)
-                itemMeta.setDisplayName(name);
-            if (lore != null)
-                itemMeta.setLore(lore);
-            if (customModelData != null)
-                itemMeta.setCustomModelData(customModelData);
-            displayClone.setItemMeta(itemMeta);
-            this.display = displayClone;
+    public Icon(@NotNull ItemStack origin, @Nullable IconDisplay iconDisplay) {
+        if (!ItemHelper.isAir(origin)) {
+            ItemStack originClone = origin.clone();
+            if (iconDisplay != null)
+                this.display = iconDisplay.applyToItemStack(originClone);
+            else
+                this.display = originClone;
         } else {
-            this.display = display;
+            this.display = origin;
         }
     }
 
---
+++ module/bukkit/ui/src/main/java/crypticlib/ui/display/IconDisplay.java
@@ -0,0 +1,123 @@
+package crypticlib.ui.display;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class IconDisplay {
+
+    protected @NotNull Material material;
+    protected @Nullable String name;
+    protected @Nullable List<String> lore;
+    protected @Nullable Integer customModelData;
+    //内部参数,请勿随意修改
+    protected @Nullable ItemStack display;
+
+    public IconDisplay(@NotNull Material material) {
+        this(material, null, null, null);
+    }
+
+    public IconDisplay(@NotNull Material material, @Nullable List<String> lore, @Nullable Integer customModelData) {
+        this(material, null, lore, customModelData);
+    }
+
+    public IconDisplay(@NotNull Material material, @Nullable String name, @Nullable Integer customModelData) {
+        this(material, name, null, customModelData);
+    }
+
+    public IconDisplay(@NotNull Material material, @Nullable Integer customModelData) {
+        this(material, null, null, customModelData);
+    }
+
+    public IconDisplay(@NotNull Material material, @Nullable List<String> lore) {
+        this(material, null, lore, null);
+    }
+
+    public IconDisplay(@NotNull Material material, @Nullable String name) {
+        this(material, name, null, null);
+    }
+
+    public IconDisplay(@NotNull Material material, @Nullable String name, @Nullable List<String> lore) {
+        this(material, name, lore, null);
+    }
+
+    public IconDisplay(@NotNull Material material, @Nullable String name, @Nullable List<String> lore, @Nullable Integer customModelData) {
+        this.material = material;
+        this.name = name;
+        this.lore = lore;
+        this.customModelData = customModelData;
+        this.display = toItemStack();
+    }
+
+    public @NotNull Material material() {
+        return material;
+    }
+
+    public IconDisplay setMaterial(@NotNull Material material) {
+        this.material = material;
+        return this;
+    }
+
+    public @Nullable String name() {
+        return name;
+    }
+
+    public IconDisplay setName(@Nullable String name) {
+        this.name = name;
+        return this;
+    }
+
+    public @Nullable List<String> lore() {
+        return lore;
+    }
+
+    public IconDisplay setLore(@Nullable List<String> lore) {
+        this.lore = lore;
+        return this;
+    }
+
+    public Integer customModelData() {
+        return customModelData;
+    }
+
+    public IconDisplay setCustomModelData(@Nullable Integer customModelData) {
+        this.customModelData = customModelData;
+        return this;
+    }
+
+    public @Nullable ItemStack display() {
+        if (display == null) {
+            display = toItemStack();
+        }
+        return display;
+    }
+
+    public @NotNull ItemStack toItemStack() {
+        ItemStack itemStack = new ItemStack(material);
+        return applyToItemStack(itemStack);
+    }
+
+    /**
+     * 将除物品类型外的参数应用到某个物品上
+     * @param itemStack 原始物品
+     * @return 修改后的物品
+     */
+    public @NotNull ItemStack applyToItemStack(@NotNull ItemStack itemStack) {
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        if (itemMeta == null)
+            throw new IllegalArgumentException("Item meta can not be null");
+        if (name != null)
+            itemMeta.setDisplayName(name);
+        if (lore != null)
+            itemMeta.setLore(lore);
+        if (customModelData != null)
+            itemMeta.setCustomModelData(customModelData);
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
+    }
+
+}
