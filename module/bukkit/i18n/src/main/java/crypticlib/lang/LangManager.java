15bc35f8a22b049c4ddfdc1ab3fb55235a06cb26
Yufiria <xiaoyi357159@gmail.com>
2024-12-11 19:36:50 +0800

    [1.8.5]LangManager现在支持替换文本内可翻译内容了

--- module/bukkit/i18n/src/main/java/crypticlib/lang/LangManager.java
+++ module/bukkit/i18n/src/main/java/crypticlib/lang/LangManager.java
@@ -2,16 +2,22 @@ package crypticlib.lang;
 
 import crypticlib.internal.PluginScanner;
 import crypticlib.lang.entry.LangEntry;
+import crypticlib.lang.entry.StringLangEntry;
 import crypticlib.lifecycle.AutoTask;
 import crypticlib.lifecycle.BukkitLifeCycleTask;
 import crypticlib.lifecycle.LifeCycle;
 import crypticlib.lifecycle.TaskRule;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @AutoTask(
     rules = {
@@ -25,6 +31,7 @@ public enum LangManager implements BukkitLifeCycleTask {
     INSTANCE;
     private final Map<String, Map<String, LangEntry<?>>> folderLangEntryMap = new ConcurrentHashMap<>();
     private final Map<String, LangEntryContainer> langEntryContainerMap = new ConcurrentHashMap<>();
+    private final Pattern langReplacePattern = Pattern.compile("<translate:([^>]+)>");
 
     public void removeLangEntryContainer(@NotNull String langFileFolder) {
         LangEntryContainer container = langEntryContainerMap.remove(langFileFolder);
@@ -50,6 +57,51 @@ public enum LangManager implements BukkitLifeCycleTask {
             folderLangEntryMap.put(langFileFolder, langEntryMap);
         }
         langEntryMap.put(entry.key(), entry);
+    }
+
+    /**
+     * 解析一段文本里的可翻译内容,并将其替换
+     * @param originStr 原始文本
+     * @param sender 用于获取语言的命令发送者
+     * @return 替换完成的文本
+     */
+    public String replaceLang(String originStr, CommandSender sender) {
+        Matcher matcher = langReplacePattern.matcher(originStr);
+        //使用 StringBuilder 来构建替换后的字符串
+        StringBuilder result = new StringBuilder();
+        //用于记录当前查找到的位置
+        int lastEnd = 0;
+
+        while (matcher.find()) {
+            //添加匹配项之前的文本
+            result.append(originStr, lastEnd, matcher.start());
+
+            //获取当前匹配到的翻译键key
+            String langKey = matcher.group(1);
+
+            String[] split = langKey.split(":");
+            if (split.length < 2) {
+                result.append(langKey);
+                continue;
+            }
+            //获取对应的翻译文本进行替换
+            LangEntry<?> langEntry = getLangEntry(split[0], String.join("", Arrays.copyOfRange(split, 1, split.length)));
+            //只有是StringLangEntry时,才能进行替换,如果找到的不是对应类型,直接不进行替换
+            if (!(langEntry instanceof StringLangEntry)) {
+                result.append(langKey);
+            } else {
+                StringLangEntry stringLangEntry = (StringLangEntry) langEntry;
+                String replacement = sender instanceof Player ? stringLangEntry.value((Player) sender) : stringLangEntry.value();
+                result.append(replacement);
+            }
+
+            //更新lastEnd以跳过已处理的匹配项
+            lastEnd = matcher.end();
+        }
+
+        //添加剩余的文本（如果有）
+        result.append(originStr.substring(lastEnd));
+        return result.toString();
     }
 
     @Override
