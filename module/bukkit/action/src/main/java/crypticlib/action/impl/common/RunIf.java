aba5a12dfb0a1ab85af32309d60db88bc644c85e
Yufiria <xiaoyi357159@gmail.com>
2025-01-10 19:54:34 +0800

    [1.9.13]runIf动作新增else字段,用于在条件不通过时执行对应动作

--- module/bukkit/action/src/main/java/crypticlib/action/impl/common/RunIf.java
+++ module/bukkit/action/src/main/java/crypticlib/action/impl/common/RunIf.java
@@ -20,27 +20,42 @@ import java.util.function.Function;
 
 /**
  * 使用placeholderapi解析condition,决定是否执行动作
- * 示例:runIf {condition_type:"placeholderapi",condition:"%player_has_permission_crypticlib.admin%",action:"command say hello"}
+ * 示例:runIf {condition_type:"placeholderapi",condition:"%player_has_permission_crypticlib.admin%",action:"command say hello",else:"tell &c你不是op,无法进行此行为"}
  * condition_type为可选项,默认为placeholderapi
  */
 public class RunIf extends BaseAction {
 
     private final String condition;
     private final Action action;
+    private final Action actionElse;
     private final Condition conditionType;
 
     public RunIf(String args) {
         Map<String, String> map = MapHelper.keyValueText2Map(Objects.requireNonNull(args));
-        this.condition = map.get("condition");
-        this.conditionType = ConditionFactory.INSTANCE.getConditionOpt(map.get("condition")).orElse(PlaceholderAPICondition.INSTANCE);
-        this.action = ActionCompiler.INSTANCE.compile(map.get("action"));
+        this.condition = map.getOrDefault("condition", "true");
+        this.conditionType = ConditionFactory.INSTANCE.getConditionOpt(map.get("condition_type")).orElse(PlaceholderAPICondition.INSTANCE);
+        if (map.containsKey("action")) {
+            this.action = ActionCompiler.INSTANCE.compile(map.get("action"));
+        } else {
+            this.action = null;
+        }
+        if (map.containsKey("else")) {
+            this.actionElse = ActionCompiler.INSTANCE.compile(map.get("else"));
+        } else {
+            this.actionElse = null;
+        }
     }
 
     @Override
     public String toActionStr() {
         Map<String, String> map = new HashMap<>();
         map.put("condition", condition);
-        map.put("action", action.toActionStr());
+        if (action != null) {
+            map.put("action", action.toActionStr());
+        }
+        if (actionElse != null) {
+            map.put("else", actionElse.toActionStr());
+        }
         map.put("condition_type", conditionType.conditionType());
         String keyValueStr = MapHelper.map2KeyValueText(map);
         return "runIf " + keyValueStr;
@@ -53,7 +68,13 @@ public class RunIf extends BaseAction {
             condition = argPreprocessor.apply(condition);
         }
         if (conditionType.test(player, condition)) {
-            action.run(player, plugin, argPreprocessor);
+            if (action != null) {
+                action.run(player, plugin, argPreprocessor);
+            }
+        } else {
+            if (actionElse != null) {
+                actionElse.run(player, plugin, argPreprocessor);
+            }
         }
         runNext(player, plugin, argPreprocessor);
     }
