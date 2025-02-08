2a9d3a9d88f23a5fd1f1d10596111124519c0bb0
Yufiria <xiaoyi357159@gmail.com>
2025-02-08 21:23:43 +0800

    [1.10.14]CollectionHelper再添加一些方法

--- platform/common/src/main/java/crypticlib/util/CollectionHelper.java
+++ platform/common/src/main/java/crypticlib/util/CollectionHelper.java
@@ -3,6 +3,7 @@ package crypticlib.util;
 import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class CollectionHelper {
 
@@ -17,22 +18,28 @@ public class CollectionHelper {
      * 判断一个集合里是否全部都不是null
      */
     public static <T> boolean isAllNotNull(Collection<T> collection) {
-        return collection.stream().anyMatch(Objects::nonNull);
+        return collection.stream().allMatch(Objects::nonNull);
     }
 
     /**
      * 获取列表中所有符合条件对象的索引
      */
-    public static <T> Set<Integer> getIndexes(List<T> list, Function<T, Boolean> equalsFunc) {
+    public static <T> Set<Integer> getIndexes(List<T> list, Function<T, Boolean> filter) {
         Set<Integer> indexes = new HashSet<>();
         for (int i = 0; i < list.size(); i++) {
-            if (equalsFunc.apply(list.get(i))) {
+            if (filter.apply(list.get(i))) {
                 indexes.add(i);
             }
         }
         return indexes;
     }
 
+    public static <T> void addIf(List<T> list, T t, BiFunction<List<T>, T, Boolean> func) {
+        if (func.apply(list, t)) {
+            list.add(t);
+        }
+    }
+    
     /**
      * 如果不包含,则添加到list里
      */
@@ -51,6 +58,37 @@ public class CollectionHelper {
             return;
         }
         list.add(t);
+    }
+
+
+    /**
+     * 添加或覆盖相等对象
+     * 如果存在多个对象,将会一并覆盖
+     */
+    public static <T> void addOrReplace(List<T> list, T t) {
+        Set<Integer> equalsIndexes = getIndexes(list, (it) -> Objects.equals(it, t));
+        if (equalsIndexes.isEmpty()) {
+            list.add(t);
+        } else {
+            equalsIndexes.forEach(index -> {
+                list.set(index, t);
+            });
+        }
+    }
+
+    /**
+     * 添加或覆盖已有符合条件对象
+     * 如果存在多个对象,会一并覆盖
+     */
+    public static <T> void addOrReplace(List<T> list, T t, BiFunction<T, T, Boolean> filter) {
+        Set<Integer> equalsIndexes = getIndexes(list, (it) -> filter.apply(it, t));
+        if (equalsIndexes.isEmpty()) {
+            list.add(t);
+        } else {
+            equalsIndexes.forEach(index -> {
+                list.set(index, t);
+            });
+        }
     }
 
     /**
