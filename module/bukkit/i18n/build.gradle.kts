182098a8f570f26b1a812220da90b23a93070868
Yufiria <xiaoyi357159@gmail.com>
2025-03-21 11:49:26 +0800

    统一一下使用的spigot版本为1.20.3

--- module/bukkit/i18n/build.gradle.kts
+++ module/bukkit/i18n/build.gradle.kts
@@ -2,7 +2,7 @@ dependencies {
     compileOnly("net.md-5:bungeecord-chat:1.16-R0.4")
     compileOnly("net.kyori:adventure-api:4.14.0")
     compileOnly("me.clip:placeholderapi:2.11.6")
-    compileOnly("org.spigotmc:spigot:1.20")
+    compileOnly("org.spigotmc:spigot:1.20.3")
     compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly(project(":platform:common"))
     compileOnly(project(":platform:bukkit"))
