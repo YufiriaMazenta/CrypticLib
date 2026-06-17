97845cee86d913dbae2017af1c59f3a46a24e32c
Yufiria <xiaoyi357159@gmail.com>
2026-06-17 19:42:38 +0800

    [1.13.19.0]TernaryFunction重命名为TriFunction

--- platform/common/src/main/java/crypticlib/util/TriFunction.java
+++ platform/common/src/main/java/crypticlib/util/TriFunction.java
@@ -6,11 +6,11 @@ import java.util.function.Function;
 /**
  * 三元函数,接受三个参数,返回一个R类型对象
  */
-public interface TernaryFunction<T, U, V, R> {
+public interface TriFunction<T, U, V, R> {
 
     R apply(T t, U u, V v);
 
-    default <X> TernaryFunction<T, U, V, X> andThen(Function<? super R, ? extends X> after) {
+    default <X> TriFunction<T, U, V, X> andThen(Function<? super R, ? extends X> after) {
         Objects.requireNonNull(after);
         return (T t, U u, V v) -> after.apply(apply(t, u, v));
     }
