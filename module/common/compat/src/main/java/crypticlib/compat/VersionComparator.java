48888bfc70b0acb5f06efc29bcb9d672171b9999
Yufiria <xiaoyi357159@gmail.com>
2026-07-15 21:33:26 +0800

    [1.19.0.1]refactor(compat): 更改VersionComparator的参数名字

--- module/common/compat/src/main/java/crypticlib/compat/VersionComparator.java
+++ module/common/compat/src/main/java/crypticlib/compat/VersionComparator.java
@@ -4,10 +4,10 @@ public interface VersionComparator {
 
     /**
      * 比较两个版本谁更新
-     * @param version1 第一个版本
-     * @param version2 第二个版本
+     * @param currentVersion 当前的版本
+     * @param compareVersion 用于比较的版本
      * @return 如果第一个版本高于第二个版本,则返回1,版本相同则返回0,第一个版本小于第二个版本则返回-1
      */
-    int compare(String version1, String version2);
+    int compare(String currentVersion, String compareVersion);
 
 }
