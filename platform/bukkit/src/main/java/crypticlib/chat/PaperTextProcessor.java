8e45922041747d137459afe26488a684457a28fc
Yufiria <xiaoyi357159@gmail.com>
2026-07-19 20:46:33 +0800

    feat(chat): PaperTextProcessor和VelocityTextProcessor增加mini message相关方法

--- platform/bukkit/src/main/java/crypticlib/chat/PaperTextProcessor.java
+++ platform/bukkit/src/main/java/crypticlib/chat/PaperTextProcessor.java
@@ -1,6 +1,7 @@
 package crypticlib.chat;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.jetbrains.annotations.Contract;
@@ -21,6 +22,7 @@ public class PaperTextProcessor {
     private static final PlainTextComponentSerializer PLAIN_SERIALIZER = PlainTextComponentSerializer
         .builder()
         .build();
+    private static final MiniMessage MINI_MESSAGE_SERIALIZER = MiniMessage.miniMessage();
 
     @Contract("null -> null")
     public static @Nullable String serializeLegacyText(Component component) {
@@ -52,6 +54,22 @@ public class PaperTextProcessor {
             return null;
         }
         return PLAIN_SERIALIZER.deserialize(text);
+    }
+
+    @Contract("null -> null")
+    public static Component deserializeMiniMessage(String text) {
+        if (text == null) {
+            return null;
+        }
+        return MINI_MESSAGE_SERIALIZER.deserialize(text);
+    }
+
+    @Contract("null -> null")
+    public static String serializeMiniMessage(Component component) {
+        if (component == null) {
+            return null;
+        }
+        return MINI_MESSAGE_SERIALIZER.serialize(component);
     }
 
 }
--- platform/velocity/src/main/java/crypticlib/chat/VelocityTextProcessor.java
+++ platform/velocity/src/main/java/crypticlib/chat/VelocityTextProcessor.java
@@ -1,6 +1,7 @@
 package crypticlib.chat;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.jetbrains.annotations.Contract;
@@ -18,6 +19,7 @@ public class VelocityTextProcessor {
     private static final PlainTextComponentSerializer PLAIN_SERIALIZER = PlainTextComponentSerializer
         .builder()
         .build();
+    private static final MiniMessage MINI_MESSAGE_SERIALIZER = MiniMessage.miniMessage();
 
     @Contract("null -> null")
     public static @Nullable String serializeLegacyText(Component component) {
@@ -49,6 +51,22 @@ public class VelocityTextProcessor {
             return null;
         }
         return PLAIN_SERIALIZER.deserialize(text);
+    }
+
+    @Contract("null -> null")
+    public static Component deserializeMiniMessage(String text) {
+        if (text == null) {
+            return null;
+        }
+        return MINI_MESSAGE_SERIALIZER.deserialize(text);
+    }
+
+    @Contract("null -> null")
+    public static String serializeMiniMessage(Component component) {
+        if (component == null) {
+            return null;
+        }
+        return MINI_MESSAGE_SERIALIZER.serialize(component);
     }
 
 }
