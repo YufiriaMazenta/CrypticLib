015a3e253bee431082f3d6375457e5e10f6e311f
Yufiria <xiaoyi357159@gmail.com>
2026-07-27 22:57:34 +0800

    build: 统一 Adventure API 版本引用

--- module/bukkit/i18n/build.gradle.kts
+++ module/bukkit/i18n/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     compileOnly("net.md-5:bungeecord-chat:1.16-R0.4")
-    compileOnly("net.kyori:adventure-api:4.14.0")
+    compileOnly("net.kyori:adventure-api:${rootProject.findProperty("platform.bukkit.adventure-api.version")}")
     compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly(project(":platform:common"))
