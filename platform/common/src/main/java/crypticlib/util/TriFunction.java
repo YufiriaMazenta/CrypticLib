9e5e136c1b6403d0d58152313462d730086bbacb
Yufiria <xiaoyi357159@gmail.com>
2026-08-22 11:00:28 +0800

    release: 1.21.14.0
    1. 新增TriCousumer和TriPredicate接口
    2. TriFunction删除几乎不使用的andThen

--- platform/common/src/main/java/crypticlib/util/TriFunction.java
+++ platform/common/src/main/java/crypticlib/util/TriFunction.java
@@ -6,13 +6,9 @@ import java.util.function.Function;
 /**
  * 三元函数,接受三个参数,返回一个R类型对象
  */
+@FunctionalInterface
 public interface TriFunction<T, U, V, R> {
 
     R apply(T t, U u, V v);
-
-    default <X> TriFunction<T, U, V, X> andThen(Function<? super R, ? extends X> after) {
-        Objects.requireNonNull(after);
-        return (T t, U u, V v) -> after.apply(apply(t, u, v));
-    }
 
 }
---
+++ platform/common/src/main/java/crypticlib/util/TriPredicate.java
@@ -0,0 +1,8 @@
+package crypticlib.util;
+
+@FunctionalInterface
+public interface TriPredicate<T, U, V> {
+
+    boolean test(T t, U u, V v);
+
+}
